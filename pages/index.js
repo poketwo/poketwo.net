d31d77df40b56cc6ae7ad0db5163ad20c4591eb5
Oliver Ni <oliver.ni@gmail.com>
2020-09-14 17:07:11 -0700

    Add trading to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
-import { faVoteYea } from "@fortawesome/free-solid-svg-icons";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import styles from "../styles/index.module.scss";
@@ -26,18 +26,18 @@ const Banner = () => (
                             href="https://invite.poketwo.net"
                         >
                             <span className="icon">
-                                <FontAwesomeIcon icon={faDiscord} />
+                                <FontAwesomeIcon icon={faRobot} />
                             </span>
                             <span>Invite Pokétwo</span>
                         </a>
                         <a
                             className="button is-medium is-light is-rounded has-shadow"
-                            href="https://top.gg/bot/716390085896962058"
+                            href="https://discord.gg/poketwo"
                         >
                             <span className="icon">
-                                <FontAwesomeIcon icon={faVoteYea} />
+                                <FontAwesomeIcon icon={faDiscord} />
                             </span>
-                            <span>Vote for us</span>
+                            <span>Join Server</span>
                         </a>
                     </div>
                 </div>
@@ -46,56 +46,62 @@ const Banner = () => (
     </div>
 );
 
-const Section1 = () => (
-    <div className="section">
-        <div className="container">
-            <p className="title is-2 has-text-centered">Features</p>
-            <div className="columns is-desktop">
-                <div className="column is-10-desktop desktop-only">
-                    <object
-                        type="image/svg+xml"
-                        data={require("../assets/mockups/catching.svg")}
-                        className="catching"
-                    >
-                        <img src={require("../assets/mockups/catching.png")} />
-                    </object>
-                </div>
-                <div className={classNames("column", styles.featurebox)}>
-                    <div className="box has-background-link-dark">
-                        <p className="title is-4">
-                            Catch &amp; Collect Pokémon
-                        </p>
-                        <div className="content">
-                            <p>
-                                As you talk in your server, pokémon will
-                                randomly appear. Compete with other server
-                                members to be the fastest to catch it!
-                            </p>
-                            <p>
-                                Keep catching to increase the size of your
-                                collection and work toward completing the
-                                Pokédex. Catch rare mythical and legendary
-                                pokémon so you can brag to your friends!
-                            </p>
-                            <p className="is-size-7 desktop-only">
-                                (Fun fact: The pokémon on this image is random!
-                                Reload the page to get a new one.)
-                            </p>
-                        </div>
-                    </div>
+const Feature = ({ filename, title, children }) => (
+    <div className="container">
+        <div className="columns is-desktop">
+            <div className="column is-10-desktop desktop-only">
+                <object
+                    type="image/svg+xml"
+                    data={require(`../assets/mockups/${filename}.svg`)}
+                    className="catching"
+                >
+                    <img src={require(`../assets/mockups/${filename}.png`)} />
+                </object>
+            </div>
+            <div className={classNames("column", styles.feature)}>
+                <div className="box has-background-link-dark">
+                    <p className="title is-4">{title}</p>
+                    <div className="content">{children}</div>
                 </div>
             </div>
         </div>
     </div>
 );
 
-const Index = () => {
-    return (
-        <>
-            <Banner />
-            <Section1 />
-        </>
-    );
-};
+const Features = () => (
+    <div className={classNames("section", styles.features)}>
+        <p className="title is-2 has-text-centered">Features</p>
+
+        <Feature filename="catching_dark" title="Catch &amp; Collect Pokémon">
+            <p>
+                As you talk in your server, pokémon will randomly appear.
+                Compete with other server members to be the fastest to catch it!
+            </p>
+            <p>
+                Keep catching to increase the size of your collection and work
+                toward completing the Pokédex. Catch rare mythical and legendary
+                pokémon so you can brag to your friends!
+            </p>
+            <p className="is-size-7 desktop-only">
+                (Fun fact: The pokémon on this image is random! Reload the page
+                to get a new one.)
+            </p>
+        </Feature>
+
+        <Feature filename="market_dark" title="Trade &amp; Sell Pokémon">
+            <p>
+                Really want a rare pokémon? Trade with other players! You can
+                also buy and sell pokémon on the global marketplace.
+            </p>
+        </Feature>
+    </div>
+);
+
+const Index = () => (
+    <>
+        <Banner />
+        <Features />
+    </>
+);
 
 export default Index;
