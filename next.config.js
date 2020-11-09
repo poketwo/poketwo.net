05a87e90a625105f3667996218a4cdafb57e5f18
Oliver Ni <oliver.ni@gmail.com>
2020-11-08 18:35:41 -0800

    Update to next 10

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@ const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([
-    [optimizedImages, { optimizeImagesInDev: true }],
+    [
+        optimizedImages,
+        {
+            optimizeImagesInDev: true,
+            responsive: {
+                adapter: require("responsive-loader/sharp"),
+            },
+        },
+    ],
 ]);
