f4407dff11fbecc71b714eaf8806692f601c974d
Oliver Ni <oliver.ni@gmail.com>
2022-05-21 17:46:55 -0700

    Add new ToS draft

---
+++ pages/terms.tsx
@@ -0,0 +1,227 @@
+const Banner = () => (
+    <div className="section">
+        <div className="container has-text-centered">
+            <p className="title is-2">Terms of Service</p>
+            <p className="subtitle">THIS IS A DRAFT</p>
+        </div>
+    </div>
+);
+
+const Content = () => (
+    <div className="content">
+        <h2>Introduction</h2>
+        <p>
+            Welcome to Pokétwo, a service provided by HFYM LLC. We're glad to have you here! These terms of service (the
+            "Terms of Service" or "Terms") are a binding contract between you and HFYM LLC (referred to as "HFYM", the
+            "Company", "we", "us", or "our"). These Terms of Service describe the rules and conditions that govern your
+            use of our website(s), product(s), bot(s), and related services (collectively, "Pokétwo" or the "Services"),
+            and hereby incorporate our Privacy Policy located at{" "}
+            <a href="https://poketwo.net/privacy">https://poketwo.net/privacy</a> (the "Privacy Policy").
+        </p>
+        <p>
+            By accepting these Terms of Service, or using or accessing our Services, you agree (i) that you are 13 years
+            of age, (ii) if you are the age of majority in your jurisdiction, that you have read, understood, and agree
+            to be legally bound by these Terms and Privacy Policy, and (iii) if you are under the age of majority in
+            your jurisdiction, that your legal guardian has reviewed and agreed to the Terms and Privacy Policy. If you
+            object to any of these terms, please do not use the Services.
+        </p>
+
+        <h2>Acceptable Use</h2>
+        <p>
+            The Services may only be used within the scope of what they are provided for, under these Terms and
+            applicable law. By accessing the Services, you agree that you shall not use the Services in a manner that:
+        </p>
+        <ul>
+            <li>
+                violates these Terms, or any applicable laws, regulations, or contractual agreements, including, when
+                relevant, the Discord Terms of Service, which can be found at{" "}
+                <a href="https://discord.com/terms">https://discord.com/terms</a>;
+            </li>
+            <li>infringes on the intellectual property rights of any party (including HFYM);</li>
+            <li>
+                is threatening, harassing, defamatory, libelous, deceptive, fraudulent, malicious, obscene, or otherwise
+                objectionable;
+            </li>
+            <li>
+                jeopardizes the security of your or any other person's account (such as exposing login credentials);
+            </li>
+            <li>
+                attempts, in any manner, to obtain passwords, accounts, or other private information from other users;
+            </li>
+            <li>interferes or attempts to interrupt the proper operation of the Services;</li>
+            <li>exploits or publicly informs other members of any error or bug which gives an unintended advantage;</li>
+            <li>involves the development, distribution, or use of unacceptable software programs (as listed below);</li>
+            <li>
+                involves the unapproved exchange of Virtual Currency or Virtual Goods (as defined below) outside the
+                Services; or
+            </li>
+            <li>condones, benefits from, or is otherwise in connection with any person who violates these Terms.</li>
+        </ul>
+        <p>
+            We reserve the right to terminate or suspend your access to the Services immediately, without prior notice,
+            and under our sole discretion, for any or no reason whatsoever, including, without limitation, a violation
+            of any of the foregoing, or any other behavior deemed a violation of the Terms of Service.
+        </p>
+        <h4>Acceptable Software Programs</h4>
+        <p>
+            The Services are intended to be used by humans only. To ensure a fair and equitable experience for all
+            users, you shall not develop, distribute, or use software programs that interact with the Services in a
+            matter that:
+        </p>
+        <ul>
+            <li>involves the automated sending of messages to influence the spawning of Pokémon ("autospamming");</li>
+            <li>involves the automated catching of Pokémon ("autocatching");</li>
+            <li>involves the automated use of the market or auction systems ("autosniping", "autobidding"); or</li>
+            <li>
+                otherwise automates interations with the Services, including the use of "selfbots" and other "auto"
+                programs.
+            </li>
+        </ul>
+
+        <h2>Virtual Currency and Virtual Goods</h2>
+        <p>
+            The Services may include opportunities to obtain virtual currency including "Pokécoins" and "Shards"
+            ("Virtual Currency") or virtual goods including "Pokémon" and "Redeems" ("Virtual Goods"). Unless otherwise
+            stated, such Virtual Currency and Virtual Goods are not tradable, exchangeable or redeemable against any
+            traditional currency, any other digital currency, goods or any other values.
+        </p>
+        <p>
+            By purchasing or obtaining Virtual Currency or Virtual Goods, you acknowledge and agree that they may only
+            be used within the Services for the purposes expressly authorized by the Company within the Services. You
+            shall not transfer, purchase, sell, or exchange such Virtual Currency or Virtual Goods outside of the
+            Services for value of any kind ("crosstrading", "third-party trading"). Examples of unacceptable behavior
+            include, but are not limited to:
+        </p>
+        <ul>
+            <li>
+                the exchange of Virtual Currency or Virtual Goods for traditional currency, such as the U.S. Dollar;
+            </li>
+            <li>the exchange of Virtual Currency or Virtual Goods for cryptocurrency, such as Bitcoin or Ethereum;</li>
+            <li>the exchange of Virtual Currency or Virtual Goods for any other paper or digital currency;</li>
+            <li>
+                the exchange of Virtual Currency or Virtual Goods for currencies, goods, and other items within
+                unapproved (as listed below) third-party services, including, but not limited to, social media
+                platforms, games, and other Discord bots;
+            </li>
+        </ul>
+        <p>
+            A violation of any of the foregoing may result in suspension of your access to the Services. The Company
+            retains the right to modify, manage, control and/or eliminate Virtual Currency and/or Virtual Goods at its
+            sole discretion.
+        </p>
+        <p>
+            Prices and availability of Virtual Goods are subject to change without notice. We shall have no liability to
+            you or any third party for the exercise of such rights. In case of contract or account suspension for any
+            cause attributable to you, any and all unused Virtual Currency and Virtual Goods shall be forfeited and no
+            refund shall be granted.
+        </p>
+        <h4>Approved Third-Party Services</h4>
+        <p>
+            Currently, the following services have been approved for third-party trading. The exchange of Virtual
+            Currency or Virtual Goods for currencies, goods, or items within services not listed below is a violation of
+            the Terms of Service.
+        </p>
+        <ul>
+            <li>P2HB (Discord bot, Application ID: 818706022675120138)</li>
+        </ul>
+
+        <h2>Terms and Conditions of Sale</h2>
+        <p>
+            While we will not charge you a fee to use the basic functionality of the Services, certain products and
+            services on the Services are provided on the basis of payment (the "Paid Services"). The fees, duration and
+            conditions applicable to the purchase of such Paid Services are described below and in the dedicated
+            sections of the Services.
+        </p>
+        <h4>Product Descriptions</h4>
+        <p>
+            Prices, descriptions or availability of products are outlined in the respective sections of the Services and
+            are subject to change without notice. While products on the Services are presented with the greatest
+            accuracy technically possible, representation on the Services through any means (including graphic material,
+            images, colors, and sounds) is for reference only and implies no warranty as to the characteristics of the
+            purchased Product. The characteristics of the chosen Product will be outlined during the purchasing process.
+        </p>
+        <h4>Prices</h4>
+        <p>
+            Stated prices on the Services may not include sales and use taxes. However, users are informed during the
+            purchasing process and before order submission, about any fees, taxes and costs that they will be charged.
+            We have the right to charge you for any taxes that we are required to pay or in fact collect related to your
+            purchase.
+        </p>
+        <h4>Billing</h4>
+        <p>
+            We use a third-party payment processor (the "Payment Processor") to bill you through a payment account
+            linked to your account on the Services (your "Billing Account"). The processing of payments will be subject
+            to the terms, conditions and privacy policies of the Payment Processor in addition to these Terms.
+            Currently, we use Stripe, Inc. as our Payment Processor. You can access Stripe's Terms of Service at{" "}
+            <a href="https://stripe.com/checkout/legal">https://stripe.com/checkout/legal</a> and their Privacy Policy
+            at <a href="https://stripe.com/privacy">https://stripe.com/privacy</a>. We are not responsible for any error
+            by, or other acts or omissions of, the Payment Processor.
+        </p>
+        <h4>Refunds</h4>
+        <p>
+            WE DO NOT GUARANTEE ANY REFUNDS OR CREDITS FOR FEES ALREADY PAID FOR THE PAID SERVICES, INCLUDING, WITHOUT
+            LIMITATION, IN THE EVENT OF YOUR TERMINATION OR CANCELLATION OF THE PAID SERVICES. WE MAY REFUSE A REFUND
+            FOR ANY OR NO REASON WHATSOEVER, AT OUR SOLE DISCRETION. WE RESERVE THE RIGHT TO MODIFY OUR REFUND POLICY AT
+            ANY TIME.
+        </p>
+
+        <h2>Liability and Indemnification</h2>
+        <h4>Disclaimer of Warranty</h4>
+        <p>
+            THE SERVICES ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+            INCLUDING, WITHOUT LIMITATION, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+            TITLE, AND NON-INFRINGEMENT, OR THAT USE OF THE SERVICES WILL BE SECURE OR ERROR-FREE OR THAT THE SERVICES
+            WILL ALWAYS FUNCTION WITHOUT DELAYS, DISRUPTIONS, OR IMPERFECTIONS. YOUR USE OF THE SERVICES IS AT YOUR SOLE
+            RISK. THE FOREGOING DISCLAIMERS SHALL APPLY TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+        </p>
+        <h4>Limitation of Liability</h4>
+        <p>
+            TO THE MAXIMUM EXTENT ALLOWED BY APPLICABLE LAW, UNDER NO CIRCUMSTANCES SHALL THE COMPANY BE LIABLE TO YOU
+            OR TO ANY OTHER PERSON FOR ANY INDIRECT, SPECIAL, INCIDENTAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND,
+            INCLUDING ANY LOSS OF PROFITS OR LOSS OF DATA, LOSS OF GOODWILL, WORK STOPPAGE, ACCURACY OF RESULTS, OR
+            COMPUTER FAILURE OR MALFUNCTION
+        </p>
+        <h4>Indemnification</h4>
+        <p>
+            You agree to indemnify and hold the Company harmless from and against any and all claims, liabilities,
+            damages (actual and consequential), losses and expenses (including attorney's fees) arising from or in any
+            way related to any claims relating to (i) your use of the Services (including any actions taken by a third
+            party using your account), and (ii) your violation of these Terms.
+        </p>
+
+        <h2>Miscellaneous</h2>
+        <h4>Service Interruption</h4>
+        <p>
+            To ensure maximum performance and level of service, the Company reserves the right to interrupt the
+            operation of the Services for maintenance at any time, informing users appropriately. The Company may also
+            decide to terminate the Services altogether at any time. If the Service are terminated, the Company will
+            cooperate with users to enable them to withdraw personal data in accordance with the law.
+        </p>
+        <p>Additionally, the Services may not be available due to reasons outside the Company's reasonable control.</p>
+        <h4>Changes to These Terms</h4>
+        <p>
+            The Company reserves the right to update these Terms of Service at any time. If our Terms change, we will
+            post a notice on our site located at <a href="https://poketwo.net">https://poketwo.net</a> and/or notify you
+            by email or other means. Unless we state otherwise, your continued use of the Services after we post
+            modifications to the Terms constitutes your agreement to those changes. If you object to the changes, please
+            do not continue using the Services.
+        </p>
+    </div>
+);
+
+const Terms = () => (
+    <>
+        <Banner />
+        <div className="section">
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-8-desktop is-offset-2-desktop">
+                        <Content />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+);
+
+export default Terms;
