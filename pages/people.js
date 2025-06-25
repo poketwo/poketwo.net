1a26056d2c50a824ef9443055c73b7f57e5bc407
Witherr <sdssouvic99@gmail.com>
2025-06-26 03:34:50 +0530

    Update people.js

--- pages/people.js
+++ pages/people.js
@@ -75,7 +75,7 @@ export async function getStaticProps(context) {
                     },
                     {
                         name: "hyperbub",
-                        image: "https://cdn.discordapp.com/avatars/361759504422928387/a_6964c3c6d24867fdaf0845c57263d3b3.gif?size=1024",
+                        image: "https://cdn.discordapp.com/avatars/361759504422928387/a_b0441250052c4e1ea035f81097d924ce.gif?size=1024",
                         secondary: "Admin\nHuman Resources Lead"
                     },
                     {
@@ -93,103 +93,71 @@ export async function getStaticProps(context) {
                 ],
                 [
                     {
-                        name: "metspek",
-                        image: "https://cdn.discordapp.com/avatars/243763234685976577/66c17b1e501d7cb4c63a783ead604cf1.png?size=1024",
-                        secondary: "Developer\nEvent Team"
-                    }
-                ],
-                [
-                    {
                         name: "skiparoo",
-                        image: "https://cdn.discordapp.com/avatars/97104885337575424/60082d42033b07aba7a746789c279ba2.png?size=1024",
+                        image: "https://cdn.discordapp.com/avatars/97104885337575424/602c68b9b032cec5bcda5ba826e0aae0.png?size=1024",
                         secondary: "Developer"
                     }
                 ],
                 [
                     {
-                        name: "ellewoods.",
-                        image: "https://cdn.discordapp.com/avatars/470615071035359262/3083017911dd33de2d803043ec16fa41.png?size=1024",
-                        secondary: "Bot Manager\nModerator"
+                        name: "lesbians.",
+                        image: "https://cdn.discordapp.com/guilds/716390832034414685/users/517063415613751337/avatars/13325936e9b88201502901967f1beeb5.png?size=1024",
+                        secondary: "Server Admin\nModerator"
+                    },
+                    {
+                        name: "tazzy989",
+                        image: "https://cdn.discordapp.com/avatars/1042169206692651038/ec2e13a27d230f8035606ab0c712deb1.png?size=1024",
+                        secondary: "Server Admin\nModerator"
+                    },
+                    {
+                        name: "rusherhey",
+                        image: "https://cdn.discordapp.com/avatars/1282634614761848832/a1ea4555da2ab6b14c4e58cf7c9c7dbd.png?size=1024",
+                        secondary: "Bot Admin\nModerator"
                     },
                     {
                         name: "bren.__.",
-                        image: "https://cdn.discordapp.com/avatars/336148113465278464/3561df33df2e935660d0cb4f023d99a8.png?size=1024",
-                        secondary: "Bot Manager\nModerator"
-                    },
-                    {
-                        name: "lesbians.",
-                        image: "https://cdn.discordapp.com/guilds/716390832034414685/users/517063415613751337/avatars/329609d042bee4d475eb87da25563b28.png?size=1024",
-                        secondary: "Server Manager\nModerator"
+                        image: "https://cdn.discordapp.com/avatars/336148113465278464/d72ac5f3b1cffeb1121800d74c017ab1.png?size=1024",
+                        secondary: "Bot Admin\nModerator"
                     }
                 ],
                 [
                     {
+                        name: "somebluepigeon",
+                        image: "https://cdn.discordapp.com/avatars/711892049842012190/1487e550da4b8beb50e0da2c5c62e4b5.png?size=1024",
+                        secondary: "Moderator\nEvent Team"
+                    },
+                    {
                         name: "frankmyocean",
-                        image: "https://cdn.discordapp.com/avatars/546492524366266369/b4fa7c3c363e1bd40bd1b32d8d9b1076.png?size=1024",
+                        image: "https://cdn.discordapp.com/avatars/546492524366266369/cc59956c246ada3de06bc90c2ceb1949.png?size=1024",
                         secondary: "Moderator\nEvent Team"
                     }
                 ],
                 [
-                    {
-                        name: "dh388jdfm38fj3",
-                        image: "https://cdn.discordapp.com/avatars/1282634614761848832/0fb5ccf932bbdab561f51f9630de631f.png?size=1024",
-                        secondary: "Moderator"
-                    },
-                    {
-                        name: "echogecko.",
-                        image: "https://cdn.discordapp.com/avatars/386482648945524738/5f64911a070b6d849cec11fdb90ee19a.png?size=1024",
-                        secondary: "Moderator"
-                    },
-                    {
-                        name: "nathuhn",
-                        image: "https://cdn.discordapp.com/avatars/344338374489931789/4db83f3d4973f03e3627b71484d6a211.png?size=1024",
-                        secondary: "Moderator"
-                    },
-                    {
-                        name: "fittlesticks",
-                        image: "https://cdn.discordapp.com/avatars/162983579536588800/3138f2b60900c97a2ef121b424ff4e25.png?size=1024",
-                        secondary: "Moderator"
-                    },
                     {
                         name: "imperatorjordy",
                         image: "https://cdn.discordapp.com/avatars/349284248840175617/b102c27457681426bedcb527528f1c18.png?size=1024",
                         secondary: "Moderator"
                     },
                     {
-                        name: "jynxerso",
-                        image: "https://cdn.discordapp.com/avatars/449792537272516628/b1fb19677d823e56026f8418fefd8924.png?size=1024",
-                        secondary: "Moderator"
-                    },
-                    {
                         name: "koh.na",
-                        image: "https://cdn.discordapp.com/avatars/1177693947481559090/6c9bd18c06c38d5b7a215a83d1be5381.png?size=1024",
+                        image: "https://cdn.discordapp.com/avatars/1177693947481559090/f385b9d0f4f00afbe4e9c68f666961fa.png?size=1024",
                         secondary: "Moderator"
                     },
                     {
-                        name: "tazzy989",
-                        image: "https://cdn.discordapp.com/avatars/1042169206692651038/57ba9756b334c213a170c622b821ec53.png?size=1024",
+                        name: "ariesv78",
+                        image: "https://cdn.discordapp.com/avatars/898949618149249085/eb3a9385b2611aeb0e2593b2d23ecc71.png?size=1024",
                         secondary: "Moderator"
                     }
                 ],
                 [
                     {
                         name: "krisppykreme",
-                        image: "https://cdn.discordapp.com/avatars/182301129231695872/a_b4dcff8caad8c7a344d83e183e51c18d.gif?size=1024",
-                        secondary: "Event Team"
-                    },
-                    {
-                        name: "a_spect",
-                        image: "https://cdn.discordapp.com/avatars/651131088219144232/2067702fd6c3fc2f19363fadce761345.png?size=1024",
-                        secondary: "Event Team"
-                    },
-                    {
-                        name: "angeljanin",
-                        image: "https://cdn.discordapp.com/avatars/874420399608332370/8a67d0a620ed11c315e2afd476e85774.png?size=1024",
+                        image: "https://cdn.discordapp.com/avatars/182301129231695872/a_85dae865457503f884c0c7fec1a94609.gif?size=1024",
                         secondary: "Event Team"
                     },
                     {
                         name: "qydv",
-                        image: "https://cdn.discordapp.com/avatars/745825974880436294/edd3c3b2993fac6e661fbc8bd9f3da7e.png?size=1024",
+                        image: "https://cdn.discordapp.com/avatars/745825974880436294/7c99269530c3c548395e5daba087b090.png?size=1024",
                         secondary: "Event Team"
                     }
                 ]
